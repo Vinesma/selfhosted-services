09936c731c1a31308a09efbd0c9dd0cc0cda2f45
Otavio Cornelio <otaviocos14@gmail.com>
2022-07-30 15:32:55 -0300

    Update up-all script to prevent mount problems

--- up-all.sh
+++ up-all.sh
@@ -2,4 +2,12 @@
 # Start all containers in this folder
 #
 
-find . -name 'docker-compose.yml' -exec sudo docker-compose -f {} up -d \;
+find . \
+    -name 'docker-compose.yml' \
+    \! -path "*/qbittorrent/*" \
+    -exec sudo docker-compose -f {} up -d \;
+
+find . \
+    -path "*/qbittorrent/*" \
+    -name 'docker-compose.yml' \
+    -exec sudo docker-compose -f {} up -d \;
