7bca0dcd444ead33b1ab75ffee0ed9fb2c86025a
Otavio Cornelio <otaviocos14@gmail.com>
2022-08-22 19:01:09 -0300

    refactor: remove qbittorrent up-all exception

--- up-all.sh
+++ up-all.sh
@@ -2,12 +2,4 @@
 # Start all containers in this folder
 #
 
-find . \
-    -name 'docker-compose.yml' \
-    \! -path "*/qbittorrent/*" \
-    -exec docker compose -f {} up -d \;
-
-find . \
-    -path "*/qbittorrent/*" \
-    -name 'docker-compose.yml' \
-    -exec docker compose -f {} up -d \;
+find . -name 'docker-compose.yml' -exec docker compose -f {} up -d \;
