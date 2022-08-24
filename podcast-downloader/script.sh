e8eaad370468c9adaf877d28d17114b84b9f25a9
Otavio Cornelio <otaviocos14@gmail.com>
2022-08-24 19:39:04 -0300

    feat: add yt-dlp script for podcast downloads

---
+++ podcast-downloader/script.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# NEEDED
+podcast_name=$1
+yt_playlist_id=$2
+token=$3
+# OPTIONAL
+sponsorblock_categories=$4
+
+podcasts_base_dir="/media/pi/Seagate Basic/Media/Podcasts/$podcast_name"
+downloader=/usr/local/bin/yt-dlp
+notification_url="https://vinesma.duckdns.org/notifications/message?token=$token"
+
+cd "$podcasts_base_dir" || { printf "%s\n" "Couldn't cd into directory: '$podcasts_base_dir'"; exit 1; }
+
+$downloader -q \
+    -x --audio-format mp3 \
+    --download-archive archive.log \
+    --sponsorblock-remove "${sponsorblock_categories:-sponsor}" \
+    --embed-thumbnail \
+    -o '%(title)s/%(title)s [%(id)s].%(ext)s' \
+    --exec "curl $notification_url -F title='Podcast Added' -F message='%(title)s' -F priority=3" \
+    "https://www.youtube.com/playlist?list=$yt_playlist_id"
