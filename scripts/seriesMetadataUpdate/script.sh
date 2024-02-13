fe3ecbefd6cc5dc48984588c41bbc6047fbd2d62
Otavio Cornelio <otaviocos14@gmail.com>
2024-02-13 11:27:55 -0300

    feat: new script for automated metadata updates

---
+++ scripts/seriesMetadataUpdate/script.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#
+# Call jellyfin api to update an ongoing series' metadata
+# IMPORTANT: series.txt needs to end on a newline.
+
+if [[ -z "$JF_SERVER_URL" && -z "$JF_API_KEY" ]]; then
+    printf "%s\n" "Error: Jellyfin environment variables not set."
+    exit 1;
+fi
+
+while IFS= read -r SERIES; do
+    series_desc=$(printf "%s" "${SERIES}" | cut -d " " -f2-)
+    series_id=$(printf "%s" "${SERIES}" | cut -d " " -f1)
+
+    curl \
+        "${JF_SERVER_URL}/Items/${series_id}/Refresh?api_key=${JF_API_KEY}&Recursive=true&ImageRefreshMode=FullRefresh&MetadataRefreshMode=FullRefresh&ReplaceAllImages=true&ReplaceAllMetadata=true" \
+        -X POST \
+        -H 'TE: trailers'
+
+    printf "%s\n" "${series_desc} updated."
+    sleep 30
+done < "series.txt"
