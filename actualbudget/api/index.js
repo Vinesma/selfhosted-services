44e0a95a593aaf7ad53b9b492eb7ae052a8e3f60
Otavio Cornelio <otaviocos14@gmail.com>
2026-07-04 14:55:46 -0300

    Update Actual api to get monthly contribution

--- actualbudget/api/index.js
+++ actualbudget/api/index.js
@@ -10,8 +10,32 @@ let api = require("@actual-app/api");
         verbose: false,
     });
 
+    const now = new Date();
+    const currentYear = now.getFullYear();
+    const currentMonth = String(now.getMonth() + 1).padStart(2, "0");
+    const yearMonth = `${currentYear}-${currentMonth}`;
+
+    const investmentsGroupName = "Investments and Savings";
+    const investmentsCategoryName = "Contributions";
+
     // This is the ID from Settings → Show advanced settings → Sync ID
     await api.downloadBudget(process.env.BUDGET_SYNC_ID);
+
+    let monthlyContribution = 0;
+    let budgetCurrentMonth = await api.getBudgetMonth(yearMonth);
+    let investmentsGroup = budgetCurrentMonth.categoryGroups.filter(
+        group => group.name === investmentsGroupName,
+    )?.[0];
+
+    if (investmentsGroup) {
+        let investmentsCategory = investmentsGroup.categories.filter(
+            category => category.name === investmentsCategoryName,
+        )?.[0];
+
+        if (investmentsCategory) {
+            monthlyContribution = investmentsCategory.balance;
+        }
+    }
 
     let accounts = await api.getAccounts();
     let balances = await Promise.all(
@@ -21,9 +45,15 @@ let api = require("@actual-app/api");
                 name: account.name,
                 balance,
             };
-        })
+        }),
     );
-    console.log(balances);
+
+    balances.push({
+        name: "monthlyContribution",
+        balance: monthlyContribution,
+    });
+
+    console.log(JSON.stringify(balances));
 
     await api.shutdown();
 })();
